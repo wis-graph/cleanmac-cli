7ac622fc1bb6cf775f4c3573e399582f1e1ebc04
wis <kogoome@kakao.com>
2026-02-25 20:36:08 +0900

    Bump formula to v1.0.2

--- formula/cleanmac.rb
+++ formula/cleanmac.rb
@@ -1,17 +1,17 @@
 class Cleanmac < Formula
   desc "A fast, AI-ready macOS system cleaner CLI with TUI"
   homepage "https://github.com/wis-graph/cleanmac-cli"
-  version "1.0.1"
+  version "1.0.2"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/wis-graph/cleanmac-cli/releases/download/v#{version}/cleanmac-aarch64-apple-darwin.tar.gz"
-      sha256 "578b34d90a8d5b677e468e8ae367b21da7589c19828260e0d0e80e035908449b"
+      sha256 "058fc534cd931fc0721bcb770f1bc43e890da2164126b83a67858eeab6d3f161"
     end
     on_intel do
       url "https://github.com/wis-graph/cleanmac-cli/releases/download/v#{version}/cleanmac-x86_64-apple-darwin.tar.gz"
-      sha256 "d1569ba4bc86c569dd408c8b9967ec4771993f8bde776206a9d0224d51369552"
+      sha256 "bf4b5ba6b12347c6eb42f5e28b67226c543af13624063f68cbd46a6c00ce9080"
     end
   end
 
